7b63d7ae0a04b68b5b42092cd028b05492f686be
elvece <lvcifferello@gmail.com>
2015-10-10 22:07:40 -0600

    backed up

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -5,16 +5,17 @@
 function isLeapYear(year){
   //if year is divisible by 100, do nothing, false
   //elseif the year is also divisible by 400, then true
-  if (year % 4 === 0){
-    if (year % 100 === 0){
-      if (year % 100 === 0 && year % 400 === 0){
-        return true;
-      }
-      return false;
-    }
+  if (year % 100 === 0 && year % 400 === 0)
+    return true;
+
+
+  if (year % 100 === 0){
+    return false;
+  }
 
   //if the year is divisible by 4, which means if it is divisible by 4, the result will not produce a fraction
 
+  if (year % 4 === 0){
     return true;
   }
 }
