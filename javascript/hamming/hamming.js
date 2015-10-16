f6babcb1809de15ee10eceb69ad55a212895efb9
elvece <lvcifferello@gmail.com>
2015-10-16 10:58:15 -0600

    aded pseudo code

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,6 +1,9 @@
 
 function compute(originalStrand, mutatedStrand){
-
+  //split each string into an array
+  //compare each index in first array to second array
+  //if differnce, add to counter
+  //return counter number
 }
 
 module.exports = compute;
