f239b066b8073b5717fea1c6652533393fb65cba
elvece <lvcifferello@gmail.com>
2015-10-19 09:29:49 -0600

    added solution with non-functional programming

--- javascript/accumulate/accumulate.js
+++ javascript/accumulate/accumulate.js
@@ -1,5 +1,13 @@
 function accumulate(input, callback){
-  return input.map(callback);
+  //solution with functional programming
+  // return input.map(callback);
+
+  //solution with non-functional programming
+  var result = [];
+  for (var i = 0; i < input.length; i++) {
+    result.push(callback(input[i]));
+  }
+  return result;
 }
 
 module.exports = accumulate;
