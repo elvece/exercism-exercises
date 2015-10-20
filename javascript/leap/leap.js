e239f8df085999c3486b2dfd5768351a71d451ae
elvece <lvcifferello@gmail.com>
2015-10-20 09:47:35 -0600

    left in commented out first iteration for reference

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,6 +1,6 @@
 //second iteration
 function isLeapYear(year){
-  return ((year % 100 !== 0 && year % 4 === 0) || year % 400 === 0)
+  return ((year % 100 !== 0 && year % 4 === 0) || year % 400 === 0);
 }
 
 module.exports = isLeapYear;
