ece80883d8cf8b39daf938aa6406428551b75b98
elvece <lvcifferello@gmail.com>
2015-10-27 09:20:27 -0600

    added bob, pseudocoded

---
+++ javascript/bob/bob.js
@@ -0,0 +1,17 @@
+//
+// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+var Bob = function() {};
+
+Bob.prototype.hey = function(input) {
+  //if includes CAPS response is Whoa chill out
+  //if includes ?, whatever
+  //if nothing, fine by that way
+  //else whatever
+
+
+};
+
+module.exports = Bob;
