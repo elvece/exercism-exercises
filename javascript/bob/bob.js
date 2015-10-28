eb6ca5a2ac9f9e236e7f565bc0c2f316e1b74a20
elvece <lvcifferello@gmail.com>
2015-10-28 10:17:37 -0600

    refactored and first three tests pass

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -3,26 +3,30 @@ var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
   //if includes CAPS response is Whoa chill out
-  //if includes ? at end, whatever
+  //if includes ? at end, sure
   //if nothing, fine by that way
   //else whatever
-  input = input.trim();
+
 //input === input.toUpperCase() && input != input.toLowerCase()
 
   if ((/(^.*[A-Z]{2,}.*$)/).test(input)) {
     return 'Whoa, chill out!';
   }
-  elseif(()){
-    //starts at end and sees if last character is question mark
-    //input.splice(-1) == '?'
 
+  //starts at end and sees if last character is question mark
+  if (input.slice(-1) === '?'){
+    return 'Sure.';
   }
   //if not blank
 //loop input if input.charAt(i) == ' '
 
+  if (input.trim() === ''){
+    return 'Fine. Be that way!';
+  }
+
   else {
     //silent
-    return 'Fine be that way.';
+    return 'Whatever.';
   }
 
 };
