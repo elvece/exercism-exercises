d62b14f9d987c042c82c9f8c17a2cfa09223f2af
elvece <lvcifferello@gmail.com>
2015-11-02 09:30:56 -0700

    first test working with if statement

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,7 +1,26 @@
 
 function words(input){
-  input = input.split(" ");
-  console.log(input)
+  var result = {};
+
+  var word = input.split(" ");
+
+  if(word.length <= 1){
+    word = word[0];
+    result.word = 1;
+  }
+  // console.log(result)
+
+  for (var i = 0; i < input.length; i++) {
+    if (input[i] === input[i + 1]){
+      result.input == input[i];
+      // word.value == ++
+      console.log(result)
+    };
+  }
+  return result;
+
 }
 
-words('test test test')
+// words('test test test hey you there')
+
+module.exports = words;
