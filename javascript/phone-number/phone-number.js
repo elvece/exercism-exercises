fcd93f12223844dee82553d56a8e9d7a8c1c0f1d
elvece <lvcifferello@gmail.com>
2015-11-03 09:29:23 -0700

    first test passes

--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -1,7 +1,14 @@
-function PhoneNumber(number){
-  this.number = number;
+function PhoneNumber(num){
+  this.num = num;
 }
 
 PhoneNumber.prototype.number = function() {
-
+  var newNumber = this.num.replace(/\D+/g, '');
+  return newNumber;
 };
+
+
+
+// if (phone.charAt(3) === "-" && phone.charAt(7) === "-" && phone.length === 12) {
+
+module.exports = PhoneNumber;
