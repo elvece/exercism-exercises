71d5bf541c489b11b990fcd30dfec8728228e537
elvece <lvcifferello@gmail.com>
2015-11-03 10:05:54 -0700

    fifth test passes

--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -6,9 +6,13 @@ PhoneNumber.prototype.number = function() {
   //replaces any non digit
   var cleanNum = this.num.replace(/\D+/g, '');
 
+  if (cleanNum.length <= 9){
+    return '0000000000';
+  }
+
   while (cleanNum.length > 10){
     //if first character is greater than 10, slice off
-    if (cleanNum.charAt(0) === "1" && cleanNum.length > 10){
+    if (cleanNum.charAt(0) === "1"){
       return cleanNum.slice(1);
     } else {
       return '0000000000';
