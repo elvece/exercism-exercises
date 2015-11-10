84f70ee05492df157c0238e7099ebfe5e1c4406b
elvece <lvcifferello@gmail.com>
2015-11-10 10:33:42 -0700

    added verse split and join, but error with test

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -1,10 +1,72 @@
-function song(){
+var oldLadySong = "
+  'I know an old lady who swallowed a fly.\nI don\'t know why she swallowed the fly. Perhaps she\'ll die.\n\n'
+  'I know an old lady who swallowed a spider.\nIt wriggled and jiggled and tickled inside her.\n'
+  'She swallowed the spider to catch the fly.\n'
+  'I don\'t know why she swallowed the fly. Perhaps she\'ll die.\n\n'
+  'I know an old lady who swallowed a bird.\n'
+  'How absurd to swallow a bird!\n'
+  'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n'
+  'She swallowed the spider to catch the fly.\n'
+  'I don\'t know why she swallowed the fly. Perhaps she\'ll die.\n\n'
+   'I know an old lady who swallowed a cat.\n'
 
-}
+  'Imagine that, to swallow a cat!\n'
+  'She swallowed the cat to catch the bird.\n'
+  'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n'
+  'She swallowed the spider to catch the fly.\n'
+  'I don\'t know why she swallowed the fly. '
+  'Perhaps she\'ll die.\n\n'
+  'I know an old lady who swallowed a dog.\n'
+  'What a hog, to swallow a dog!\n'
+  'She swallowed the dog to catch the cat.\n'
+  'She swallowed the cat to catch the bird.\n'
+  'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n'
+  'She swallowed the spider to catch the fly.\n'
+  'I don\'t know why she swallowed the fly. '
+  'Perhaps she\'ll die.\n\n'
+  'I know an old lady who swallowed a goat.\n'
+  'Just opened her throat and swallowed a goat!\n'
+  'She swallowed the goat to catch the dog.\n'
+  'She swallowed the dog to catch the cat.\n'
+  'She swallowed the cat to catch the bird.\n'
+  'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n'
+  'She swallowed the spider to catch the fly.\n'
+  'I don\'t know why she swallowed the fly. '
+  'Perhaps she\'ll die.\n\n'
+  'I know an old lady who swallowed a cow.\n'
+  'I don\'t know how she swallowed a cow!\n'
+  'She swallowed the cow to catch the goat.\n'
+  'She swallowed the goat to catch the dog.\n'
+  'She swallowed the dog to catch the cat.\n'
+  'She swallowed the cat to catch the bird.\n'
+  'She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n'
+  'She swallowed the spider to catch the fly.\n'
+  'I don\'t know why she swallowed the fly. '
+  'Perhaps she\'ll die.\n\n'
+  'I know an old lady who swallowed a horse.\n'
+  'She\'s dead, of course!\n\n'";
 
-song.prototype.verse = function(num) {
 
-
+var Song = function(lyrics) {
+  this.verseArray = lyrics.split('\n\n');
 };
 
-module.exports = song;
+Song.prototype.verse = function(start) {
+  return this.verseArray[start - 1] + '\n';
+};
+
+Song.prototype.verses = function(start, end) {
+  var sliced = this.verseArray.slice(start - 1, end);
+  return sliced.join('\n\n') + '\n\n';
+};
+
+Song.prototype.sing = function() {
+  var end = this.verseArray.length;
+  return this.verses(1, end);
+};
+
+Song.oldLadySong = function() {
+  return new Song(oldLadySong);
+};
+
+module.exports = Song.oldLadySong();
