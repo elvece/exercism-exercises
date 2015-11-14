34d98ce94128028967b9378ac42272fe53186356
elvece <lvcifferello@gmail.com>
2015-11-14 15:41:01 -0700

    fixed this.name original string

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -2,18 +2,18 @@
 var usedNames = [];
 
 function Robot(){
-  this.name = 'A';
+  this.name = '';
 
-  var numbers = ['1','2','3','4','5','6','7','8','9','10'];
+  var numbers = ['1','2','3','4','5','6','7','8','9','0'];
   var letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
   while(usedNames.indexOf(this.name) === -1) {
-    for (var i = 1; i < 2; i++) {
-      this.name += letters[Math.floor(Math.random() * letters.length)]
+    for (var i = 0; i < 2; i++) {
+      this.name += letters[Math.floor(Math.random() * letters.length)];
     }
 
     for (var i = 0; i < 3; i++) {
-      this.name += numbers[Math.floor(Math.random() * numbers.length)]
+      this.name += numbers[Math.floor(Math.random() * numbers.length)];
     }
       usedNames.push(this.name)
   }
