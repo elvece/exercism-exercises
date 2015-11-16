699967ce7127cd0c954cff5651f43c6b42e8cf6f
elvece <lvcifferello@gmail.com>
2015-11-16 14:03:25 -0700

    refactored second iteration with for each

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -1,37 +1,35 @@
-
-function transform(obj){
-  var newSet = {};
-
-    var newValue = Object.keys(obj);
-
-    for (newValue in obj){
-      var set = obj[newValue];
-      for (var i = 0; i < set.length; i++) {
-        newSet[set[i].toLowerCase()] = parseInt(newValue);
-      }
-    }
-
-  return newSet;
-}
-
-
-
+//first iteration
 // function transform(obj){
 //   var newSet = {};
 
-//   //Object.keys(obj) = [ '1', '2', '3', '4', '5', '8', '10' ]
-//   Object.keys(obj).forEach(function(element, index, array){
+//     var newValue = Object.keys(obj);
 
-//     var set = parseInt(element);
-//     // console.log(set)
+//     for (newValue in obj){
+//       var set = obj[newValue];
+//       for (var i = 0; i < set.length; i++) {
+//         newSet[set[i].toLowerCase()] = parseInt(newValue);
+//       }
+//     }
 
-//     array.forEach(function(letter){
-//       newSet[letter.toLowerCase()] = set;
-//     });
-
-// });
-//     return newSet;
+//   return newSet;
 // }
+
+
+//second iteration
+function transform(obj){
+  var newSet = {};
+
+  //Object.keys(obj) = [ '1', '2', '3', '4', '5', '8', '10' ]
+  Object.keys(obj).forEach(function(element, index, array){
+    var score = parseInt(element);
+    var set = obj[element];
+
+    set.forEach(function(element, index, array){
+      newSet[element.toLowerCase()] = score;
+    });
+});
+    return newSet;
+}
 
 
 module.exports = transform;
