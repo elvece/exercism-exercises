bc05fd5723ad1de761cbb71aa014a5203817371d
elvece <lvcifferello@gmail.com>
2015-11-16 08:59:08 -0700

    pseudo coded problem

---
+++ javascript/etl/etl.js
@@ -0,0 +1,16 @@
+//old: key to value object, which is array
+//new: key to value object, lowercase letter to point number
+//anything that initially has a key of 1, switch it to the value of each letter in the array
+//for each value in the array, make the index a key and the value the old key
+
+function transform(obj){
+  //get first key in entered object
+  //loop through the array at this key
+  //make each index a key in the new object
+  //make the key of the initial key/value pair the new value of each index
+  //for each key, loop through array and make each index have the value of the key
+  var newSet = {};
+
+
+  };
+}
