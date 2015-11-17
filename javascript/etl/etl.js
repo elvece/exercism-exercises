e7aa920f3b5be9cf2af3bd91e77e5c36a4f6ddfc
elvece <lvcifferello@gmail.com>
2015-11-16 21:42:32 -0700

    refactored with for each loops

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -1,31 +1,29 @@
-//first iteration
-// function transform(obj){
-//   var newSet = {};
+// first iteration
+function transform(obj){
+  var newSet = {};
 
-//     var newValue = Object.keys(obj);
+    var newValue = Object.keys(obj);
 
-//     for (newValue in obj){
-//       var set = obj[newValue];
-//       for (var i = 0; i < set.length; i++) {
-//         newSet[set[i].toLowerCase()] = parseInt(newValue);
-//       }
-//     }
+    for (newValue in obj){
+      var set = obj[newValue];
+      for (var i = 0; i < set.length; i++) {
+        newSet[set[i].toLowerCase()] = parseInt(newValue);
+      }
+    }
 
-//   return newSet;
-// }
+  return newSet;
+}
 
 
 //second iteration
 function transform(obj){
   var newSet = {};
 
-  //Object.keys(obj) = [ '1', '2', '3', '4', '5', '8', '10' ]
-  Object.keys(obj).forEach(function(element, index, array){
-    var score = parseInt(element);
-    var set = obj[element];
+  Object.keys(obj).forEach(function(num){
+    var score = parseInt(num);
 
-    set.forEach(function(element, index, array){
-      newSet[element.toLowerCase()] = score;
+    obj[num].forEach(function(letter){
+      newSet[letter.toLowerCase()] = score;
     });
 });
     return newSet;
