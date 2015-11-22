58434be28c8ef2d86b500766ac2c2f1dccf206eb
elvece <lvcifferello@gmail.com>
2015-11-21 21:15:57 -0700

    added pseudo code:

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -2,4 +2,16 @@
 
 // A gigasecond is one billion (10**9) seconds.
 
+function Gigasecond(date) {
+  this.birthday = date;
+}
+
+
+Gigasecond.prototype.date= function() {
+  //return date of your gs anniversary
+
+};
+
+//calculate how many seconds from the entered date
+//convert date into seconds
 //
