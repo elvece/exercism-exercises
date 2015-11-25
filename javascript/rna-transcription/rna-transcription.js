b5eb65b8ae2b4bc97f78323805f586d3a8f42249
elvece <lvcifferello@gmail.com>
2015-11-25 08:27:45 -0500

    set up prototype

---
+++ javascript/rna-transcription/rna-transcription.js
@@ -0,0 +1,13 @@
+// Write a program that, given a DNA strand, returns its RNA complement (per RNA transcription).
+// * `G` -> `C`
+// * `C` -> `G`
+// * `T` -> `A`
+// * `A` -> `U`
+
+function DnaTranscriber(){}
+
+DnaTranscriber.prototype.toRna = function(nuc) {
+
+};
+
+module.exports = DnaTranscriber;
