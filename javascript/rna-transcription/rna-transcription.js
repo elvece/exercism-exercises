ee19e6748393d4e2ccc8630f6862d9007fe83ee5
elvece <lvcifferello@gmail.com>
2015-11-25 22:21:42 -0500

    first set of tests for rna transcription pass

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -6,19 +6,63 @@
 
 function DnaTranscriber(){}
 
-DnaTranscriber.prototype.toRna = function(nuc) {
-  if(nuc === 'C'){
-    return nuc = 'G';
+DnaTranscriber.prototype.toRna = function(dna) {
+  var arr = dna.split("");
+  // var rna;
+  // console.log('nuc: ' + nuc)
+  //go down one at a time not whole thing
+  for (var i = 0; i < arr.length; i++) {
+    console.log('arr: ' +arr)
+    if(arr[i] === 'A'){
+      arr[i] = 'U';
+    }
+    if(arr[i] === 'T'){
+      arr[i] = 'A';
+    }
+    if(arr[i] === 'C'){
+      arr[i] = 'x';
+    }
+    if(arr[i] === 'G'){
+      arr[i] = 'C';
+    }
+    if(arr[i] === 'x'){
+      arr[i] = 'G';
+    }
   }
-  if(nuc === 'G'){
-    return nuc = 'C';
-  }
-  if(nuc === 'T'){
-    return nuc = 'A';
-  }
-  if(nuc === 'A'){
-    return nuc = 'U';
-  }
-};
+    var rna = arr.join("");
+    return rna;
+
+    // var rna = dna.map(function(nuc){
+    //   var copy = nuc;
+    //   console.log('copy: ' + copy)
+    //   nuc[nuc.indexOf('G')] ='C';
+    //   nuc[nuc.indexOf('C')] ='G';
+    //   nuc[nuc.indexOf('T')] ='A';
+    //   nuc[nuc.indexOf('A')] ='U';
+    //   return copy;
+    // });
+
+    // nuc[i] === 'A' ? 'U' : nuc[i];
+    // nuc[i] ==='T'? 'A' : nuc[i];
+    // nuc[i] === 'G' ? 'C' : nuc[i];
+    // nuc[i] === 'C' ? 'G' : nuc[i];
+  // }
+
+  // nuc[nuc.indexOf('G')] ='C';
+  // nuc[nuc.indexOf('C')] ='G';
+  // nuc[nuc.indexOf('T')] ='A';
+  // nuc[nuc.indexOf('A')] ='U';
+
+  // var rna = nuc.map(function(nuc){
+  //   nuc === 'A' ? 'U' : nuc;
+  //   nuc ==='T'? 'A' : nuc;
+  //   nuc === 'G' ? 'C' : nuc;
+  //   nuc === 'C' ? 'G' : nuc;
+  // });
+
+  // rna.join("");
+  // console.log('rna: ' + rna);
+  // return rna;
+}
 
 module.exports = DnaTranscriber;
