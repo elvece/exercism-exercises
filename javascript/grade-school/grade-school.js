dcdd3813cefe8870216783175e59431f5099673b
elvece <lvcifferello@gmail.com>
2015-12-30 20:47:31 -0700

    refactored add function

--- javascript/grade-school/grade-school.js
+++ javascript/grade-school/grade-school.js
@@ -5,13 +5,11 @@ function School(){
   };
   this.add = function(name, grade){
     if (!roster.hasOwnProperty(grade)){
-      var arr = [];
-      roster[grade] = arr;
-      arr.push(name);
-    } else {
+      roster[grade] = [];
+      roster[grade].push(name);
+    } else
       roster[grade].push(name);
       roster[grade].sort();
-    }
   };
   this.grade = function(grade){
     var gradeRoster;
