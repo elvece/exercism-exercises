ebe7444a4446f6aa434bc38203950515ed72c501
elvece <lvcifferello@gmail.com>
2016-01-06 17:28:41 -0700

    refactor

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -4,13 +4,12 @@ function Hamming(){
   this.compute = function(originalStrand, mutatedStrand){
     var mutatedLength = mutatedStrand.length,
         originalLength = originalStrand.length,
-        length = Math.min(mutatedLength, originalLength),
         count = 0;
 
-        if(mutatedLength !== originalLength)
+        if (mutatedLength !== originalLength)
           throw 'DNA strands must be of equal length.';
 
-        for (var i = 0; i < length; i++) {
+        for (var i = 0; i < mutatedLength; i++) {
           if(mutatedStrand[i] !== originalStrand[i])
             count ++;
         }
