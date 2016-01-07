535bbec78a6e4a02a77a21802c907f645bcf9afd
elvece <lvcifferello@gmail.com>
2016-01-06 17:24:34 -0700

    added error conditional, all tests pass

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -2,12 +2,17 @@
 function Hamming(){
 
   this.compute = function(originalStrand, mutatedStrand){
-    var length = Math.min(mutatedStrand.length, originalStrand.length),
+    var mutatedLength = mutatedStrand.length,
+        originalLength = originalStrand.length,
+        length = Math.min(mutatedLength, originalLength),
         count = 0;
+
+        if(mutatedLength !== originalLength)
+          throw 'DNA strands must be of equal length.';
+
         for (var i = 0; i < length; i++) {
-          if(mutatedStrand[i] !== originalStrand[i]){
+          if(mutatedStrand[i] !== originalStrand[i])
             count ++;
-          }
         }
         return count;
   };
